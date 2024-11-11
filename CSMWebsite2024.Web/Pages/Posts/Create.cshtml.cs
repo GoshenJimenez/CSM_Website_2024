448dd813fc8363f1e9c3e1dbf5dbdd2fdd06a5b1
Goshen Jimenez <gjimenez@csmbataan.ph.education>
2024-11-11 17:29:10 +0800

    Navigation changes

--- CSMWebsite2024.Web/Pages/Posts/Create.cshtml.cs
+++ CSMWebsite2024.Web/Pages/Posts/Create.cshtml.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace CSMWebsite2024.Web.Pages
+namespace CSMWebsite2024.Web.Pages.Posts
 {
-    public class IndexModel : PageModel
+    public class Create : PageModel
     {
         public void OnGet()
         {
