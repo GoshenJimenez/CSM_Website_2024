b749ccef5062e71b4b1f8f055b71e259092bfc56
Goshen Jimenez <gjimenez@csmbataan.ph.education>
2024-11-11 17:53:06 +0800

    Adding data to the database

--- CSMWebsite2024.Web/Pages/Posts/Create.cshtml.cs
+++ CSMWebsite2024.Web/Pages/Posts/Create.cshtml.cs
@@ -1,3 +1,5 @@
+using CSMWebsite2024.Contracts.Posts;
+using CSMWebsite2024.Data.Posts;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,50 @@ namespace CSMWebsite2024.Web.Pages.Posts
 {
     public class Create : PageModel
     {
+        private readonly ILogger<Index> _logger;
+        private readonly IPostService _postService;
+
+        public Create(ILogger<Index> logger, IPostService postService)
+        {
+            _logger = logger;
+            _postService = postService;
+        }
+
+
+        [BindProperty]
+        public Post? Item { get; set; }
+
         public void OnGet()
         {
+            this.Item = new Post();
         }
+
+        public async Task OnPost()
+        {
+            if (this.Item == null)
+            {
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Item.Title))
+            {
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Item.Description))
+            {
+                return;
+            }
+
+            this.Item.Id = Guid.NewGuid();
+            this.Item.AuthorId = Guid.NewGuid();
+            this.Item.UpdatedAt = DateTime.Now;
+            this.Item.CreatedAt = DateTime.Now;
+
+            await _postService.AddAsync(this.Item);
+
+            RedirectPermanent("~/posts");
+        }
+
     }
 }
